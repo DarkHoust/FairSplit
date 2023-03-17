da805d48d101cc73a045dc90072730bc4fc5bcb4
DarkHost <sultanisk@mail.ru>
2023-03-17 10:57:05 +0600

    Initial Commit

---
+++ SomeWorks/SomeWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeWorksApp.swift
+//  SomeWorks
+//
+//  Created by Sultan on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SomeWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
