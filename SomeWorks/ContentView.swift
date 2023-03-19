55e39d1a5b86db7312229cd33a99946077d8c34a
DarkHost <sultanisk@mail.ru>
2023-03-19 11:11:09 +0600

    Publishing files

--- SomeWorks/ContentView.swift
+++ SomeWorks/ContentView.swift
@@ -8,19 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    let percentage = [0,5,10,15,20]
+    @State private var personNumber = 0
+    @State private var currentCost = 0.00
+    @State private var currentTip = 0
+    @FocusState private var amountIsFocused: Bool
+    
+    var separateCost : Double {
+        let peopleCount = Double(personNumber + 2)
+        let tip = Double(currentTip)
+        
+        let tipValue = (currentCost / 100) * tip
+        let grandTotal = tipValue + currentCost
+        let separateCost = grandTotal / peopleCount
+        
+        return separateCost
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    var totalCost: Double{
+        let tip = Double(currentTip)
+        
+        let tipValue = (currentCost / 100) * tip
+        let grandTotal = tipValue + currentCost
+        
+        return grandTotal
+    }
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Price", value: $currentCost, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of People", selection: $personNumber){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                } header: {
+                    Text("Current information")
+                }
+                Section{
+                    Picker("Tip percentage", selection: $currentTip){
+                        ForEach(percentage, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Percentage of Tip")
+                }
+                
+                Section{
+                    Text(separateCost, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Cost for each person")
+                }
+                
+                Section{
+                    Text(totalCost, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Total Cost")
+                }
+            }
+            .navigationTitle("FairSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
